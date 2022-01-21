8db5004d8d040b276da6e23018974f1948435a70
Thomas Shallenberger <twshal@gmail.com>
2022-01-21 10:32:33 -0600

    [chore] serve static assets from client/dist

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,10 +1,16 @@
 import { Module } from '@nestjs/common';
-import { AppController } from './app.controller';
+import { ServeStaticModule } from '@nestjs/serve-static';
+import { join } from 'path';
 import { AppService } from './app.service';
 
 @Module({
-  imports: [],
-  controllers: [AppController],
+  imports: [
+    ServeStaticModule.forRoot({
+      rootPath: join(__dirname, '..', 'client/dist'),
+      exclude: ['/api*'],
+    }),
+  ],
+  controllers: [],
   providers: [AppService],
 })
 export class AppModule {}
