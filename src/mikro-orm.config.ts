3f78e8d564cbd6455c59c799294871d0d0e4d5a7
Thomas Shallenberger <thomas@drop-engine.com>
2022-03-09 19:57:31 +0000

    Merged PR 35: [ci] Modified the mikro-orm.config.ts to use proper clientUrl, driverOptions,...
    [ci] Modified the mikro-orm.config.ts to use proper clientUrl, driverOptions, and migration options for CI

--- src/mikro-orm.config.ts
+++ src/mikro-orm.config.ts
@@ -2,17 +2,22 @@ import { DbProduct } from "@catalog/domain/entities/Product.entity";
 import { DbProductType } from "@catalog/domain/entities/ProductType.entity";
 import { DbProductVariant } from "@catalog/domain/entities/ProductVariant.entity";
 import { Options } from "@mikro-orm/core";
+import { PostgreSqlDriver } from "@mikro-orm/postgresql";
 import { DbUser } from "@users/domain/entities/User.entity";
 
-const config: Options = {
+const config: Options<PostgreSqlDriver> = {
   entities: [DbProduct, DbProductType, DbProductVariant, DbUser],
-  clientUrl: process.env.DATABASE_URL,
-  dbName: process.env.POSTGRES_DB || "dropengine",
-  host: process.env.POSTGRES_HOST || "localhost",
-  port: parseInt(process.env.POSTGRES_PORT) || 5432,
-  user: process.env.POSTGRES_USER || "postgres",
-  password: process.env.POSTGRES_PASSWORD || "password",
-  schema: process.env.POSTGRES_SCHEMA || "public",
+  clientUrl: process.env.DATABASE_URL || undefined,
+  driverOptions: {
+    connection: { ssl: true, wrap: false },
+  },
+  migrations: { disableForeignKeys: false },
+  // dbName: process.env.POSTGRES_DB || "dropengine",
+  // host: process.env.POSTGRES_HOST || "localhost",
+  // port: parseInt(process.env.POSTGRES_PORT) || 5432,
+  // user: process.env.POSTGRES_USER || "postgres",
+  // password: process.env.POSTGRES_PASSWORD || "password",
+  // schema: process.env.POSTGRES_SCHEMA || "public",
   type: "postgresql",
   debug: true,
 };
