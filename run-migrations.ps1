ead20f3f8a4329a90e8a5920aa0d51b322ba3958
Thomas Shallenberger <thomas@drop-engine.com>
2022-03-09 18:34:49 +0000

    Merged PR 31: [ci] modified PR build to publish artifact with all migrations, resources, postman tests, and dist
    [ci] modified PR build to publish artifact with all migrations, resources, postman tests, and dist

--- run-migrations.ps1
+++ run-migrations.ps1
@@ -1,6 +1,16 @@
+Get-ChildItem -Path "./_artifact" -Recurse | Remove-Item -Force -Recurse
+New-Item -Path "./" -Name "_artifact" -ItemType "directory" -Force
+New-Item -Path "./_artifact" -Name "src" -ItemType "directory" -Force
+New-Item -Path "./_artifact" -Name "postman" -ItemType "directory" -Force
+New-Item -Path "./_artifact" -Name "migrations" -ItemType "directory" -Force
+New-Item -Path "./_artifact" -Name "azure" -ItemType "directory" -Force
+New-Item -Path "./_artifact" -Name "dist" -ItemType "directory" -Force
 
-New-Item -Path "./" -Name "_migrations" -ItemType "directory" -Force
-New-Item -Path "./_migrations" -Name "dist" -ItemType "directory" -Force
+Copy-Item -Path "./src/*" -Destination "./_artifact/src" -Recurse -Force
+Copy-Item -Path "./postman/*" -Destination "./_artifact/postman" -Recurse -Force
+Copy-Item -Path "./migrations/*" -Destination "./_artifact/migrations" -Recurse -Force
+Copy-Item -Path "./azure/*" -Destination "./_artifact/azure" -Recurse -Force
+Copy-Item -Path "./dist/*" -Destination "./_artifact/dist" -Recurse -Force
 
-Copy-Item -Path "./dist/migrations/*" -Destination "./_migrations/dist" -Recurse -Force
-Copy-Item -Path "./package.json" -Destination "./_migrations"
+Copy-Item -Path "./package.json" -Destination "./_artifact"
+Copy-Item -Path "./tsconfig.json" -Destination "./_artifact"
