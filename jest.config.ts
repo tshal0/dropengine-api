75abfc57a078ccdec52c2effede55568f2668ef2
Thomas Shallenberger <thomas@drop-engine.com>
2022-03-10 00:23:03 +0000

    Merged PR 37: [ci] added coverage test to CI pipeline
    [ci] added coverage test to CI pipeline

--- jest.config.ts
+++ jest.config.ts
@@ -1,31 +1,31 @@
-import type { Config } from '@jest/types';
-import mapper from 'jest-module-name-mapper';
+import type { Config } from "@jest/types";
+import mapper from "jest-module-name-mapper";
 const config: Config.InitialOptions = {
-  moduleFileExtensions: ['js', 'json', 'ts'],
+  moduleFileExtensions: ["js", "json", "ts"],
   moduleNameMapper: {
     ...mapper(),
   },
-  modulePaths: ['src'],
-  testRegex: '.*\\.spec\\.ts$',
+  modulePaths: ["src"],
+  testRegex: ".*\\.spec\\.ts$",
   transform: {
-    '^.+\\.(t|j)s$': 'ts-jest',
+    "^.+\\.(t|j)s$": "ts-jest",
   },
-  collectCoverageFrom: ['src/**/*.(t|j)s'],
-  coveragePathIgnorePatterns: ['src/console', 'src/migration'],
-  coverageDirectory: 'coverage',
-  testEnvironment: 'node',
-  moduleDirectories: ['node_modules', 'src'],
-  rootDir: './',
+  collectCoverageFrom: ["src/**/*.(t|j)s"],
+  coveragePathIgnorePatterns: ["src/console", "src/migration"],
+  coverageDirectory: "coverage",
+  coverageReporters: ["json", "lcov", "text", "clover", "cobertura"],
+  testEnvironment: "node",
+  moduleDirectories: ["node_modules", "src"],
+  rootDir: ".",
 
-  // collectCoverage: true,
-  reporters: ['default', 'jest-junit'],
-  json: true,
+  collectCoverage: true,
+  reporters: ["default", "jest-junit"],
   globals: {
-    'ts-jest': {
-      tsconfig: 'tsconfig.test.build.json',
+    "ts-jest": {
+      tsconfig: "tsconfig.test.build.json",
     },
   },
-  setupFiles: ['./jest-setup-file.ts'],
+  setupFiles: ["./jest-setup-file.ts"],
 };
 
 export default config;
